cd2a1c90d039b015913b99f622464bdb4ef61bb5
PARTH RAJAURIA <59495763+ParthRajauria@users.noreply.github.com>
2024-05-28 18:48:51 +0000

    adding module 2

---
+++ 02-experiment_tracking/train.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+import click
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "parth")
+        
+        mlflow.log_param("algoname", "random forest")  
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        print('rf min samples split:- ', rf.min_samples_split)
+
+
+if __name__ == '__main__':
+    run_train()
